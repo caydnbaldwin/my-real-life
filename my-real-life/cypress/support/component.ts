778532e382bba2136ed00fa352307c20b1b4bab4
caydnbaldwin <caydnbaldwin@gmail.com>
2025-08-29 12:42:07 -0600

    Updated environment.

---
+++ my-real-life/cypress/support/component.ts
@@ -0,0 +1,9 @@
+// ***********************************************
+// Component testing support file for Cypress
+// ***********************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Import global styles or setup needed for component tests
+import '../../src/app/globals.css'
